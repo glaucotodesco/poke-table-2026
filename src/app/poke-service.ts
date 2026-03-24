7e668ffceb6489b05259d051451cd7df48259dd3
glaucotodesco <glauco.todesco@gmail.com>
2026-03-24 13:48:39 -0300

    load all pokemons

--- src/app/poke-service.ts
+++ src/app/poke-service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin, range } from 'rxjs';
+import { map, mergeMap, toArray } from 'rxjs/operators';
 import { Pokemon } from './pokemon';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PokeService {
-   apiUrl = "https://pokeapi.co/api/v2/pokemon";
+  apiUrl = 'https://pokeapi.co/api/v2/pokemon';
 
-   constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-   getPokemons(): Observable<Pokemon[]>{
-      return this.http.get<Pokemon[]>(this.apiUrl);
+  getPokemons(): Observable<Pokemon[]> {
+    return range(1, 168).pipe(
+      mergeMap(id => this.http.get<Pokemon>(`${this.apiUrl}/${id}`)),
+      toArray()
+    );
   }
 }
